51d2b50321d311afc3b8c20d9f1a3674bec03a3d
chrik5 <christian.kanzian@gmx.at>
2016-04-07 16:22:07 +0200

    [selection_to_pdf] Added check button to rotate portrait images

--- official/selection_to_pdf.lua
+++ official/selection_to_pdf.lua
@@ -42,10 +42,16 @@ dt.preferences.register
 local title_widget = dt.new_widget("entry") {
   placeholder="Title"
 }
+
+local autorotate_widget = dt.new_widget("check_button"){
+  label = "rotate portrait images for better fitting", value = false
+}
+
 local widget = dt.new_widget("box") {
   orientation=horizontal,
   dt.new_widget("label"){label = "Title:"},
-  title_widget
+  title_widget,
+  autorotate_widget
 }
 
 local ending = [[
@@ -83,7 +89,7 @@ local function thumbnail(latexfile,i,image,file)
   local ext=string.gsub(file, "(.*)(%..*)", "%2")
   my_write(latexfile,"\\begin{minipage}[b]{"..width.."\\textwidth}\n")
   
-  if image.height > image.width then 
+  if autorotate_widget.value == true and image.height > image.width then 
     my_write(latexfile,"\\includegraphics[angle=90, width=\\textwidth]{{"..filenoext.."}"..ext.."}\\newline\n")
    else
     my_write(latexfile,"\\includegraphics[width=\\textwidth]{{"..filenoext.."}"..ext.."}\\newline\n")
