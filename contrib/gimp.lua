9693b017c427ee6df917773fba5c61a0b1e31cb2
Bill Ferguson <bill@moonshinecomputers.com>
2016-04-10 16:27:49 -0400

    Changed back to string.format in show_status but coded it correctly this time.

--- contrib/gimp.lua
+++ contrib/gimp.lua
@@ -120,7 +120,7 @@ end
 
 local function show_status(storage, image, format, filename,
   number, total, high_quality, extra_data)
-  dt.print(_("Exporting to gimp "..tostring(number).."/"..tostring(total)))
+    dt.print(string.format(_("Export Image %i/%i"), number, total))
 end
 
 local function gimp_edit(storage, image_table, extra_data) --finalize
