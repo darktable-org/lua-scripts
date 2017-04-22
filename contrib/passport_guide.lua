c8d5e9d9d2b9537865dc5c43f41033a4e793e7be
khampf <k.hampf.net@gmail.com>
2017-04-22 22:06:55 +0300

    Longer version of GPL text

--- contrib/passport_guide.lua
+++ contrib/passport_guide.lua
@@ -1,4 +1,23 @@
 --[[
+  Passport cropping guide for darktable
+
+  copyright (c) 2017  Kåre Hampf
+  
+  darktable is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  darktable is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+--[[
 PASSPORT CROPPING GUIDE
 guides for cropping passport photos based on documents from the Finnish police
 (https://www.poliisi.fi/instancedata/prime_product_julkaisu/intermin/embeds/poliisiwwwstructure/38462_Passikuvaohje_EN.pdf) describing passport photo dimensions of 47x36 mm and 500x653 px for digital biometric data stored in passports. They use ISO 19794-5 standard based on ICAO 9303 regulations which should also be compliant for all of Europe.
@@ -17,10 +36,6 @@ INSTALLATION
 USAGE
 * when using the cropping tool, select "passport" as guide and if you added the line in yout rc
   select "passport 36x47mm" as aspect
-
-LICENSE
-GPLv2
-
 ]]
 
 local dt = require "darktable"
