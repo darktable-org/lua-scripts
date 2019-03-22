85390e663cad03afcf026773c1843b5981b5bb1e
BzKevin <36381472+BzKevin@users.noreply.github.com>
2019-03-22 09:37:08 -0400

    Make text Translatable
    Also updates the Usage guidelines up top
    Also added protection in case user enters a negative number in the qty programs box

--- contrib/OpenInProg.lua
+++ contrib/OpenInProg.lua
@@ -29,7 +29,12 @@ External programs
 Install: (see here for more detail: https://github.com/darktable-org/lua-scripts )
  1) Copy this file in to your "lua/contrib" folder where all other scripts reside. 
  2) Require this file in your luarc file, as with any other dt plug-in: require "contrib/OpenInProg"
-On the initial startup go to darktable settings > lua options and set your executable paths and other preferences, then restart darktable
+
+ Use the configurator found in the bottom left of lightable view to setup each program and what controls should be generated for each button
+ In the top entry box enter the number of programs you would like to configure, this can be increased or decreased at any time by entering a new number and hitting refresh
+ All settings will be saved and auto-loaded and controls generated at dt startup
+ If you add new programs you can click the 'generate' button to create the new control buttons immediately for all newly added programs (avoiding having to restart dt)
+ If you want to retain settings for a program, but don't want the controls generated you can de-select the 'enable' option and this program will be bypassed whenever generating controls (either at startup or via the button)
 
 ]]
 
@@ -41,6 +46,14 @@ local mod = 'module_OpenInProg'
 local os_path_separator = '/'
 if dt.configuration.running_os == 'windows' then os_path_separator = '\\' end
 du.check_min_api_version("5.0.0", "enfuseAdvanced") 
+
+-- Tell gettext where to find the .mo file translating messages for a particular domain
+local gettext = dt.gettext
+gettext.bindtextdomain("OpenInProg",dt.configuration.config_dir.."/lua/locale/")
+local function _(msgid)
+    return gettext.dgettext("OpenInProg", msgid)
+end
+
 local temp
 local programs = {}
 local index = {}
@@ -80,12 +93,12 @@ local function NewProgram(index) -- creates a new program object and initializes
         temp = string.gsub(temp, ' ', '')
         local name_is_valid = #temp >= 1
         name_is_valid = name_is_valid and not string.match(temp,'^program')
-        if self.GUI.change_name.label == 'change name' then --user requested to change program name, lock all other controls
+        if self.GUI.change_name.label == _('change name') then --user requested to change program name, lock all other controls
             sense = false
-            self.GUI.change_name.label = 'save name'
+            self.GUI.change_name.label = _('save name')
         elseif name_is_valid then --user requested to save new name and the new name is valid, update name, save all settings to pref with new name, update the program chooser list, unlock all other controls
             sense = true
-            self.GUI.change_name.label = 'change name'
+            self.GUI.change_name.label = _('change name')
             self.name = self.GUI.name.text
             dt.preferences.write(mod, 'prog'..index, 'string', self.name)
             dt.preferences.write(mod, self.name..'bin', 'string', self.GUI.bin.value)
@@ -98,7 +111,7 @@ local function NewProgram(index) -- creates a new program object and initializes
             GUI.show_programs[index] = self.name
         else --user requested to save a new name which is not valid
             sense = false
-            dt.print('new name is not valid')
+            dt.print(_('new name is not valid'))
         end
         self.GUI.name.editable = not sense
         self.GUI.bin.sensitive = sense
@@ -125,7 +138,7 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
     
     this.GUI.change_name = dt.new_widget('button'){
-        label = 'save name',
+        label = _('save name'),
         clicked_callback = function() this:manage_sensitivity() end
     }
     
@@ -143,9 +156,9 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
     
     this.GUI.open_all = dt.new_widget('check_button'){
-        label = 'create "open all" button', 
+        label = _('create "open all" button'), 
         value = dt.preferences.read(mod, this.name..'openAll', 'bool'),
-        tooltip ='create a button that will enable the "open all" behavior for specified program',
+        tooltip =_('create a button that will enable the "open all" behavior for specified program'),
         sensitive = false,
         clicked_callback = function(self) 
             dt.preferences.write(mod, this.name..'openAll', 'bool', self.value) 
@@ -154,9 +167,9 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
 
     this.GUI.open_each = dt.new_widget('check_button'){
-        label = 'create "open each" button', 
+        label = _('create "open each" button'), 
         value = dt.preferences.read(mod, this.name..'openEach', 'bool'),
-        tooltip ='create a button that will enable the "open each" behavior for specified program',
+        tooltip =_('create a button that will enable the "open each" behavior for specified program'),
         sensitive = false,
         clicked_callback = function(self) 
             dt.preferences.write(mod, this.name..'openEach', 'bool', self.value) 
@@ -165,9 +178,9 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
     
     this.GUI.export_all = dt.new_widget('check_button'){
-        label = 'create "export all" button', 
+        label = _('create "export all" button'), 
         value = dt.preferences.read(mod, this.name..'exportAll', 'bool'),
-        tooltip ='create a button that will enable the "export all" behavior for specified program',
+        tooltip =_('create a button that will enable the "export all" behavior for specified program'),
         sensitive = false,
         clicked_callback = function(self) 
             dt.preferences.write(mod, this.name..'exportAll', 'bool', self.value) 
@@ -176,9 +189,9 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
         
     this.GUI.enable = dt.new_widget('check_button'){
-        label = 'enable',
+        label = _('enable'),
         value = dt.preferences.read(mod, this.name..'enable', 'bool'),
-        tooltip = 'generate this programs specified actions on startup or when generate is clicked',
+        tooltip = _('generate this programs specified actions on startup or when generate is clicked'),
         sensitive = false,
         clicked_callback = function(self) 
             dt.preferences.write(mod, this.name..'enable', 'bool', self.value) 
@@ -211,9 +224,9 @@ local function NewProgram(index) -- creates a new program object and initializes
     }
     
     this.GUI.source_loc = dt.new_widget('check_button'){
-        label = 'save to source location',
+        label = _('save to source location'),
         value = dt.preferences.read(mod, this.name..'source', 'bool'),
-        tooltip ='generate this programs specified actions on startup or when generate is clicked',
+        tooltip =_('generate this programs specified actions on startup or when generate is clicked'),
         sensitive = false,
         clicked_callback = function(self) 
             dt.preferences.write(mod, this.name..'source', 'bool', self.value) 
@@ -229,12 +242,12 @@ end
 GUI.stack = dt.new_widget('stack'){}
 
 GUI.s_label = dt.new_widget('section_label'){
-    label = 'program options'
+    label = _('program options')
 }
 
 GUI.show_programs = dt.new_widget('combobox'){
-    label = "show program",
-    tooltip = "show options for specified program",
+    label = _("show program"),
+    tooltip = _("show options for specified program"),
     changed_callback = function(self)
         GUI.stack.active = self.selected
     end 
@@ -248,7 +261,7 @@ GUI.qty_prog_entry = dt.new_widget('entry'){
 
 local function UpdateProgramList() -- updates the stack to contain only the number of programs specified, updates program chooser list as well
     local temp = #programs
-    local qty_programs = tonumber(GUI.qty_prog_entry.text)
+    local qty_programs = math.max(tonumber(GUI.qty_prog_entry.text),0)
     dt.preferences.write(mod, 'qty_programs', 'integer', qty_programs)
     if temp < qty_programs then --add programs
         for j = (temp + 1), qty_programs, 1 do
@@ -273,8 +286,8 @@ local function pre_call(prog) --checks if program exists (only on initial call),
     if prog.first_run then
         prog.bin = df.check_if_bin_exists(prog.name)
         if not prog.bin then
-            dt.print_error(prog.name..' not found')
-            dt.print('ERROR - '..prog.name..' not found')
+            dt.print_error(prog.name.._(' not found'))
+            dt.print(_('ERROR - ')..prog.name.._(' not found'))
             prog.install_error = true
         end
         prog.first_run = false
@@ -295,11 +308,11 @@ end
 local function Func_OpenAll(ButtonText) --parses button text to determine what program object to use while executing, attempts to open all images in a single call
     local idx = index[string.sub(string.match(ButtonText, ': .*'), 3)]  --looks at button text to determine what button was pressed and it's associated program index
     if pre_call(programs[idx]) then
-        dt.print('resolve install issue with '..programs[idx].name)
+        dt.print(_('resolve install issue with ')..programs[idx].name)
         return
     end
     
-    dt.print('opening all in '..programs[idx].name)
+    dt.print(_('opening all in ')..programs[idx].name)
     
     local images = dt.gui.selection()
     local curr_image = ""
@@ -313,18 +326,18 @@ local function Func_OpenAll(ButtonText) --parses button text to determine what p
     local run_cmd = build_execute_command(programs[idx])
     local resp = dsys.external_command(run_cmd)
     if resp ~= 0 then
-        dt.print_error('an error occured while trying to open images in '..programs[idx].name)
+        dt.print_error(_('an error occured while trying to open images in ')..programs[idx].name)
     end
 end
 
 local function Func_OpenEach(ButtonText) --parses button text to determine what program object to use while executing, attempts to open each image in a unique call
     local idx = index[string.sub(string.match(ButtonText, ': .*'), 3)]
     if pre_call(programs[idx]) then
-        dt.print('resolve install issue with '..programs[idx].name)
+        dt.print(_('resolve install issue with ')..programs[idx].name)
         return
     end
         
-    dt.print('opening each in '..programs[idx].name)
+    dt.print(_('opening each in ')..programs[idx].name)
     
     local images = dt.gui.selection()
     local curr_image = ''
@@ -336,7 +349,7 @@ local function Func_OpenEach(ButtonText) --parses button text to determine what 
         local run_cmd = build_execute_command(programs[idx])
         local resp = dsys.external_command(run_cmd, true)
         if resp ~= 0 then
-            dt.print_error('an error occured while trying to open images in '..programs[idx].name)
+            dt.print_error(_('an error occured while trying to open images in ')..programs[idx].name)
         end 
     end
 end
@@ -344,16 +357,16 @@ end
 local function Func_Export(storage, image_table, extra_data) --uses storage name to determine what program object to use while executing, attempts to export all images, move them to designated folder, then open those all in a single call
     local idx = index[storage.name]
     if pre_call(programs[idx]) then
-        dt.print('resolve install issue with '..programs[idx].name)
+        dt.print(_('resolve install issue with ')..programs[idx].name)
         return
     end 
 
     if (programs[idx].GUI.temp_loc.value == nil or programs[idx].GUI.temp_loc.value == '') and not programs[idx].GUI.source_loc.value then   --Check that an output path is selected
-        dt.print('ERROR: no target directory selected')
+        dt.print(_('ERROR: no target directory selected'))
         return
     end
     dt.preferences.write(mod, programs[idx].name..'loc', 'string', programs[idx].GUI.temp_loc.value)
-    dt.print("Opening exported images in "..programs[idx].name)
+    dt.print(_("Opening exported images in ")..programs[idx].name)
     local images_to_open = ''
     for source_image,temp_path in pairs(image_table) do
         local new_path = programs[idx].GUI.temp_loc.value
@@ -369,7 +382,7 @@ local function Func_Export(storage, image_table, extra_data) --uses storage name
 end
 
 local function show_status(storage, image, format, filename, number, total, high_quality, extra_data) --shows how many images are being exported
-    dt.print('exporting '..tostring(number)..'/'..tostring(total))   
+    dt.print(_('exporting ')..tostring(number)..'/'..tostring(total))   
 end
 
 local function GenerateProgramControls() --generates user spcified controls for each program object that exists
@@ -412,14 +425,14 @@ local function GenerateProgramControls() --generates user spcified controls for 
 end
 
 GUI.refresh = dt.new_widget('button'){
-    label = 'refresh',
-    tooltip = 'refresh list of programs available',
+    label = _('refresh'),
+    tooltip = _('refresh list of programs available'),
     clicked_callback = function() UpdateProgramList() end
 }
 
 GUI.generate = dt.new_widget('button'){
-    label =  'generate controls',
-    tooltip = 'generate buttons for all programs. If new programs have been added they will be generated now, if you have removed programs they will no longer appear after a restart of dt',
+    label =  _('generate controls'),
+    tooltip = _('generate buttons for all programs. If new programs have been added they will be generated now, if you have removed programs they will no longer appear after a restart of dt'),
     clicked_callback = function() GenerateProgramControls() end
 }
 
@@ -428,7 +441,7 @@ GenerateProgramControls()
 
 dt.register_lib(
     mod,
-    'Open In Program Configurator',
+    _('Open In Program Configurator'),
     true,
     false,
     {[dt.gui.views.lighttable] = {"DT_UI_CONTAINER_PANEL_LEFT_BOTTOM", 100}},
