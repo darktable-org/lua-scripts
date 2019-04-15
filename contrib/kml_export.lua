a383d1ebf8b7195a6e0ea449b0e926f16c4eb5e2
supertobi <tobias.jakobs@gmail.com>
2019-04-15 15:48:31 +0200

    Update kml_export.lua
    Works with Windows

--- contrib/kml_export.lua
+++ contrib/kml_export.lua
@@ -20,17 +20,14 @@
 darktable KML export script
 
 ADDITIONAL SOFTWARE NEEDED FOR THIS SCRIPT
-* mkdir
-* zip (only if you create KMZ files)
-* convert (ImageMagick)
-* xdg-open
-* xdg-user-dir
+* zip (at the moment Linux only and only if you create KMZ files)
+* magick (ImageMagick)
+* xdg-user-dir (Linux)
 
 WARNING
 This script is only tested with Linux
 
 USAGE
-* require script "official/yield" from your main Lua file in the first line
 * require this script from your main Lua file
 * when choosing file format, pick JPEG or PNG as Google Earth doesn't support other formats
 
@@ -39,12 +36,14 @@ USAGE
 local dt = require "darktable"
 local du = require "lib/dtutils"
 local df = require "lib/dtutils.file"
-require "official/yield"
+local ds = require "lib/dtutils.string"
+local dsys = require "lib/dtutils.system"
+
 local gettext = dt.gettext
 
 local PS = dt.configuration.running_os == "windows" and "\\" or "/"
 
-du.check_min_api_version("3.0.0", kml_export) 
+du.check_min_api_version("5.0.0", kml_export) 
 
 -- Tell gettext where to find the .mo file translating messages for a particular domain
 gettext.bindtextdomain("kml_export",dt.configuration.config_dir.."/lua/locale/")
@@ -53,166 +52,8 @@ local function _(msgid)
   return gettext.dgettext("kml_export", msgid)
 end
 
--- Sort a table
-local function spairs(_table, order) -- Code copied from http://stackoverflow.com/questions/15706270/sort-a-table-in-lua
-  -- collect the keys
-  local keys = {}
-  for _key in pairs(_table) do keys[#keys + 1] = _key end
-
-  -- if order function given, sort by it by passing the table and keys a, b,
-  -- otherwise just sort the keys
-  if order then
-    table.sort(keys, function(a,b) return order(_table, a, b) end)
-  else
-    table.sort(keys)
-  end
-
-  -- return the iterator function
-  local i = 0
-  return function()
-    i = i + 1
-    if keys[i] then
-      return keys[i], _table[keys[i]]
-    end
-  end
-end
-
 local function show_status(storage, image, format, filename, number, total, high_quality, extra_data)
   dt.print(string.format(_("Export Image %i/%i"), number, total))
-end
-
--- Strip accents from a string
--- Copied from https://forums.coronalabs.com/topic/43048-remove-special-characters-from-string/
-function string.stripAccents( str )
-  local tableAccents = {}
-  -- A
-  tableAccents["à"] = "a"
-  tableAccents["À"] = "A"
-  tableAccents["á"] = "a"
-  tableAccents["Á"] = "A"
-  tableAccents["â"] = "a"
-  tableAccents["Â"] = "A"
-  tableAccents["ã"] = "a"
-  tableAccents["Ã"] = "A"
-  tableAccents["ä"] = "a"
-  tableAccents["Ä"] = "A"
-  -- B
-  -- C
-  tableAccents["ç"] = "c"
-  tableAccents["Ç"] = "C"
-  tableAccents["č"] = "c"
-  tableAccents["Č"] = "C"
-  -- D
-  tableAccents["ď"] = "d"
-  tableAccents["Ď"] = "d"
-  -- E
-  tableAccents["è"] = "e"
-  tableAccents["È"] = "E"
-  tableAccents["é"] = "e"
-  tableAccents["É"] = "E"
-  tableAccents["ê"] = "e"
-  tableAccents["Ê"] = "E"
-  tableAccents["ë"] = "e"
-  tableAccents["Ë"] = "E"
-  tableAccents["ě"] = "e"
-  tableAccents["Ě"] = "E"
-  -- F
-  -- G
-  -- H
-  -- I
-  tableAccents["ì"] = "i"
-  tableAccents["Ì"] = "I"
-  tableAccents["í"] = "i"
-  tableAccents["Í"] = "I"
-  tableAccents["î"] = "i"
-  tableAccents["Î"] = "I"
-  tableAccents["ï"] = "i"
-  tableAccents["Ï"] = "I"
-  -- J
-  -- K
-  -- L
-  tableAccents["ĺ"] = "l"
-  tableAccents["Ĺ"] = "L"
-  tableAccents["ľ"] = "l"
-  tableAccents["Ľ"] = "L"
-  -- M
-  -- N
-  tableAccents["ñ"] = "n"
-  tableAccents["Ñ"] = "N"
-  tableAccents["ň"] = "n"
-  tableAccents["Ň"] = "N"
-  -- O
-  tableAccents["ò"] = "o"
-  tableAccents["Ò"] = "O"
-  tableAccents["ó"] = "o"
-  tableAccents["Ó"] = "O"
-  tableAccents["ô"] = "o"
-  tableAccents["Ô"] = "O"
-  tableAccents["õ"] = "o"
-  tableAccents["Õ"] = "O"
-  tableAccents["ö"] = "o"
-  tableAccents["Ö"] = "O"
-  -- P
-  -- Q
-  -- R
-  tableAccents["ŕ"] = "r"
-  tableAccents["Ŕ"] = "R"
-  tableAccents["ř"] = "r"
-  tableAccents["Ř"] = "R"
-  -- S
-  tableAccents["š"] = "s"
-  tableAccents["Š"] = "S"
-  -- T
-  tableAccents["ť"] = "t"
-  tableAccents["Ť"] = "T"
-  -- U
-  tableAccents["ù"] = "u"
-  tableAccents["Ù"] = "U"
-  tableAccents["ú"] = "u"
-  tableAccents["Ú"] = "U"
-  tableAccents["û"] = "u"
-  tableAccents["Û"] = "U"
-  tableAccents["ü"] = "u"
-  tableAccents["Ü"] = "U"
-  tableAccents["ů"] = "u"
-  tableAccents["Ů"] = "U"
-  -- V
-  -- W
-  -- X
-  -- Y
-  tableAccents["ý"] = "y"
-  tableAccents["Ý"] = "Y"
-  tableAccents["ÿ"] = "y"
-  tableAccents["Ÿ"] = "Y"
-  -- Z
-  tableAccents["ž"] = "z"
-  tableAccents["Ž"] = "Z"
-
-  local normalizedString = ""
-
-  for strChar in string.gmatch(str, "([%z\1-\127\194-\244][\128-\191]*)") do
-    if tableAccents[strChar] ~= nil then
-      normalizedString = normalizedString..tableAccents[strChar]
-    else
-      normalizedString = normalizedString..strChar
-    end
-  end
-
-  return normalizedString
-end
-
--- Escape XML characters
--- Keep &amp; first, otherwise it will double escape other characters
--- https://stackoverflow.com/questions/1091945/what-characters-do-i-need-to-escape-in-xml-documents
-function string.escapeXmlCharacters( str )
-
-  str = string.gsub(str,"&", "&amp;")
-  str = string.gsub(str,"\"", "&quot;")
-  str = string.gsub(str,"'", "&apos;")
-  str = string.gsub(str,"<", "&lt;")
-  str = string.gsub(str,">", "&gt;")
-
-  return str
 end
 
 -- Add duplicate index to filename
@@ -230,41 +71,34 @@ function addDuplicateIndex( index, filename )
 end
 
 local function create_kml_file(storage, image_table, extra_data)
-  if not df.check_if_bin_exists("mkdir") then
-    dt.print_error(_("mkdir not found"))
-    return
-  end
-  if not df.check_if_bin_exists("convert") then
-    dt.print_error(_("convert not found"))
-    return
-  end
-  if not df.check_if_bin_exists("xdg-open") then
-    dt.print_error(_("xdg-open not found"))
-    return
-  end
-  if not df.check_if_bin_exists("xdg-user-dir") then
-    dt.print_error(_("xdg-user-dir not found"))
-    return
-  end
 
-  dt.print_error("Will try to export KML file now")
+  local magickPath = dt.preferences.read("kml_export","magickPath","string")
+  if not df.check_if_bin_exists(magickPath) then
+    dt.print_error(_("magick not found"))
+    return
+  end
+  if dt.configuration.running_os == "linux" then
+    if not df.check_if_bin_exists("xdg-user-dir") then
+      dt.print_error(_("xdg-user-dir not found"))
+      return
+    end
+  end
+  dt.print_log("Will try to export KML file now")
 
   local imageFoldername
-  if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true ) then
+  if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true 
+       and dt.configuration.running_os == "linux") then
     if not df.check_if_bin_exists("zip") then
       dt.print_error(_("zip not found"))
       return
     end
-
     exportDirectory = dt.configuration.tmp_dir
     imageFoldername = ""
   else
     exportDirectory = dt.preferences.read("kml_export","ExportDirectory","string")
-
     -- Creates dir if not exsists
     imageFoldername = "files"..PS
-    local mkdirCommand = "mkdir -p "..exportDirectory..PS..imageFoldername
-    dt.control.execute(mkdirCommand)
+    df.mkdir(df.sanitize_filename(exportDirectory..PS..imageFoldername))
   end
 
   -- Create the thumbnails
@@ -275,15 +109,17 @@ local function create_kml_file(storage, image_table, extra_data)
       local path, filename, filetype = string.match(exported_image, "(.-)([^\\/]-%.?([^%.\\/]*))$")
       filename = string.upper(string.gsub(filename,"%.%w*", ""))
 
-      -- convert -size 92x92 filename.jpg -resize 92x92 +profile "*" thumbnail.jpg
+      -- magick -size 92x92 filename.jpg -resize 92x92 +profile "*" thumbnail.jpg
       -- In this example, '-size 120x120' gives a hint to the JPEG decoder that the image is going to be downscaled to
       -- 120x120, allowing it to run faster by avoiding returning full-resolution images to  GraphicsMagick for the
       -- subsequent resizing operation. The '-resize 120x120' specifies the desired dimensions of the output image. It
       -- will be scaled so its largest dimension is 120 pixels. The '+profile "*"' removes any ICM, EXIF, IPTC, or other
       -- profiles that might be present in the input and aren't needed in the thumbnail.
 
-      local convertToThumbCommand = "convert -size 96x96 "..exported_image.." -resize 92x92 -mattecolor \"#FFFFFF\" -frame 2x2 +profile \"*\" "..exportDirectory..PS..imageFoldername.."thumb_"..filename..".jpg"
-      dt.control.execute(convertToThumbCommand)
+      local convertToThumbCommand = ds.sanitize(magickPath) .. " -size 96x96 "..exported_image.." -resize 92x92 -mattecolor \"#FFFFFF\" -frame 2x2 +profile \"*\" "..exportDirectory..PS..imageFoldername.."thumb_"..filename..".jpg"
+
+      df.file_copy(exported_image, exportDirectory..PS..imageFoldername..filename.."."..filetype)
+	  dsys.external_command(convertToThumbCommand)
     else
       -- Remove exported image if it has no GPS data
       os.remove(exported_image)
@@ -294,7 +130,19 @@ local function create_kml_file(storage, image_table, extra_data)
 
     -- Strip accents from the filename, because GoogleEarth can't open them
     -- https://github.com/darktable-org/lua-scripts/issues/54
-    filmName = string.stripAccents(filmName)
+    filmName = ds.strip_accents(filmName)
+
+	-- Remove chars we don't like to have in filenames
+	filmName = string.gsub(filmName, [[\]], "") 
+	filmName = string.gsub(filmName, [[/]], "")
+	filmName = string.gsub(filmName, [[:]], "") 
+	filmName = string.gsub(filmName, [["]], "")
+	filmName = string.gsub(filmName, "<", "") 
+	filmName = string.gsub(filmName, ">", "") 
+	filmName = string.gsub(filmName, "|", "")
+	filmName = string.gsub(filmName, "*", "")
+	filmName = string.gsub(filmName, "?", "")
+	filmName = string.gsub(filmName,'[.]', "") -- At least Windwows has problems with the "." and the start command
   end
 
   exportKMLFilename = filmName..".kml"
@@ -324,12 +172,12 @@ local function create_kml_file(storage, image_table, extra_data)
 
       local image_title, image_description
       if (image.title and image.title ~= "") then
-        image_title = string.escapeXmlCharacters(image.title)
+        image_title = ds.escape_xml_characters(image.title)
       else
         image_title = filename..extension
       end
       -- Characters should not be escaped in CDATA, but we are using HTML fragment, so we must escape them
-      image_description = string.escapeXmlCharacters(image.description)
+      image_description = ds.escape_xml_characters(image.description)
 
       kml_file = kml_file.."    <name>"..image_title.."</name>\n"
       kml_file = kml_file.."    <description>"..image_description.."</description>\n"
@@ -374,7 +222,7 @@ local function create_kml_file(storage, image_table, extra_data)
     kml_file = kml_file.."    <LineString>\n"
     kml_file = kml_file.."      <coordinates>\n"
 
-    for image,exported_image in spairs(image_table, function(t,a,b) return b.exif_datetime_taken > a.exif_datetime_taken end) do
+    for image,exported_image in du.spairs(image_table, function(t,a,b) return b.exif_datetime_taken > a.exif_datetime_taken end) do
       if ((image.longitude and image.latitude) and
         (image.longitude ~= 0 and image.latitude ~= 90) -- Sometimes the north-pole but most likely just wrong data
          ) then
@@ -397,13 +245,15 @@ local function create_kml_file(storage, image_table, extra_data)
   kml_file = kml_file.."</kml>"
 
   local file = io.open(exportDirectory..PS..exportKMLFilename, "w")
+
   file:write(kml_file)
   file:close()
 
   dt.print("KML file created in "..exportDirectory)
 
   -- Compress the files to create a KMZ file
-  if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true ) then
+  if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true 
+       and dt.configuration.running_os == "linux") then
     exportDirectory = dt.preferences.read("kml_export","ExportDirectory","string")
 
     local createKMZCommand = "zip --test --move --junk-paths "
@@ -428,40 +278,65 @@ local function create_kml_file(storage, image_table, extra_data)
 
   -- Open the file with the standard programm
   if ( dt.preferences.read("kml_export","OpenKmlFile","bool") == true ) then
-    local kmlFileOpenCommand
+    local path
 
-    if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true ) then
-      kmlFileOpenCommand = "xdg-open "..exportDirectory..PS.."\""..exportKMZFilename.."\""
+    if ( dt.preferences.read("kml_export","CreateKMZ","bool") == true
+	     and dt.configuration.running_os == "linux") then
+      path = exportDirectory..PS..exportKMZFilename
     else
-      kmlFileOpenCommand = "xdg-open "..exportDirectory..PS.."\""..exportKMLFilename.."\""
+      path = exportDirectory..PS..exportKMLFilename
     end
-    dt.control.execute(kmlFileOpenCommand)
+
+    dsys.launch_default_app(df.sanitize_filename(path))
   end
 
 end
 
 -- Preferences
-dt.preferences.register("kml_export",
-  "OpenKmlFile",
-  "bool",
-  _("KML export: Open KML/KMZ file after export"),
-  _("Opens the KML file after the export with the standard program for KML files"),
-  false )
-
-local handle = io.popen("xdg-user-dir DESKTOP")
-local result = handle:read()
-if (result == nil) then
-  result = ""
+if dt.configuration.running_os == "windows" then
+  dt.preferences.register("kml_export",
+    "OpenKmlFile",
+    "bool",
+    _("KML export: Open KML file after export"),
+    _("Opens the KML file after the export with the standard program for KML files"),
+    false )
+else
+  dt.preferences.register("kml_export",
+    "OpenKmlFile",
+    "bool",
+    _("KML export: Open KML/KMZ file after export"),
+    _("Opens the KML file after the export with the standard program for KML files"),
+    false )
 end
-handle:close()
+
+local defaultDir = ''
+if dt.configuration.running_os == "windows" then
+  defaultDir = os.getenv("USERPROFILE")
+elseif dt.configuration.running_os == "macos" then
+  defaultDir =  os.getenv("home")
+else
+  local handle = io.popen("xdg-user-dir DESKTOP")
+  defaultDir = handle:read()
+  handle:close()
+end
+
 
 dt.preferences.register("kml_export",
   "ExportDirectory",
   "directory",
   _("KML export: Export directory"),
   _("A directory that will be used to export the KML/KMZ files"),
-  result )
+  defaultDir )
 
+if dt.configuration.running_os ~= "linux" then  
+  dt.preferences.register("kml_export", 
+    "magickPath",	-- name
+	"file",	-- type
+	_("KML export: ImageMagick binary Location"),	-- label
+	_("Install location of magick[.exe]. Requires restart to take effect."),	-- tooltip
+	"magick")	-- default
+end  
+  
 dt.preferences.register("kml_export",
   "CreatePath",
   "bool",
@@ -469,15 +344,21 @@ dt.preferences.register("kml_export",
   _("connect all images with a path"),
   false )
 
-dt.preferences.register("kml_export",
-  "CreateKMZ",
-  "bool",
-  _("KML export: Create KMZ file"),
-  _("Compress all imeges to one KMZ file"),
-  true )
+if dt.configuration.running_os == "linux" then  
+  dt.preferences.register("kml_export",
+    "CreateKMZ",
+    "bool",
+    _("KML export: Create KMZ file"),
+    _("Compress all imeges to one KMZ file"),
+    true )
+end
 
 -- Register
-dt.register_storage("kml_export", _("KML/KMZ Export"), nil, create_kml_file)
+if dt.configuration.running_os == "windows" then
+  dt.register_storage("kml_export", _("KML Export"), nil, create_kml_file)
+else
+  dt.register_storage("kml_export", _("KML/KMZ Export"), nil, create_kml_file)
+end
 
 -- vim: shiftwidth=2 expandtab tabstop=2 cindent syntax=lua
 -- kate: hl Lua;
