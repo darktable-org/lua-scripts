b620c49f851224ba40db36ec4edb44c5da2c04f7
Bill Ferguson <wpferguson@gmail.com>
2020-08-23 14:49:45 -0400

    On further thought, there are only two scenarios, a script file is modified or a translation file is modified.  For the first scenario I combined tools/create_potfiles.sh, tools/fix_header.sh, and tools/update_po_files.sh into tools/update_translations.sh.
    So, when a script file is updated then tools/update_translations.sh
    and tools/create_mo.sh need to be run.  When only a transltion
    file is updated, then just tools/create_mo.sh need to be run.

--- tools/create_potfiles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find ../../ -name "*.lua" -print | xargs grep -l bindtextdomain > POTFILES.in
--- tools/fix_header.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for i in `cat LINGUAS` ; do perl -p -i -e 's/PACKAGE VERSION/darktable lua-scripts/' $i/* ; perl -p -i -e "s/guage: \\\n/guage: $i\\\n/" $i/* ; perl -p -i -e 's/CHARSET/UTF-8/' $i/* ; done
