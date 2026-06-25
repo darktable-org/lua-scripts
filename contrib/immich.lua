edf73ee2f4231ea5cf8be2197f8a504305a5a5ef
Colin Holzman <colin@holzman.ch>
2026-06-25 12:31:58 -0400

    add to copyright line

--- contrib/immich.lua
+++ contrib/immich.lua
@@ -1,6 +1,7 @@
 --[[
   This file is part of darktable,
   copyright (c) 2024 Giorgio Massussi
+  copyright (c) 2026 Colin Holzman
 
   darktable is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
